fb7e812973398fab68ea0fb9bc5fef65d3ba29ad
HaiwangYu <yuhw.pku@gmail.com>
2025-11-19 13:43:57 -0600

    update

--- dune10kt-1x2x6/run.sh
+++ dune10kt-1x2x6/run.sh
@@ -1,4 +1,4 @@
-# time lar -n 10 -c gen_genie.fcl -o gen.root >& gen.log
-# time lar -n 10 -c g4.fcl -s gen.root -o g4.root >& g4.log
+time lar -n 10 -c gen_genie.fcl -o gen.root >& gen.log
+time lar -n 10 -c g4.fcl -s gen.root -o g4.root >& g4.log
 time lar -n 10 -c wcls_sim_sp.fcl -s g4.root -o sp.root >& sp.log
 time lar -n 10 -c wcls-labelling2d.fcl -s sp.root --no-output >& labelling2d.log
