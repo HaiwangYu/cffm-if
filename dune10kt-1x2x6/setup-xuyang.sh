0fe0c4a05e57daf3337f7c97e41f21f235a810b0
HaiwangYu <yuhw.pku@gmail.com>
2026-06-05 18:06:31 -0500

    save cfg using xuyang's setup

---
+++ dune10kt-1x2x6/setup-xuyang.sh
@@ -0,0 +1,6 @@
+source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+setup dunesw v10_20_08d00 -q e26:prof
+unsetup wirecell
+setup wirecell v0_36_1 -q e26:prof
+# source /exp/dune/data/users/xning/larsoft/v10_20_08d00/localProducts_larsoft_v10_20_08_e26_prof/setup
+# mrbslp
